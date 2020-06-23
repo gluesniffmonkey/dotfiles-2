1b420063b867340e2a7f6d9b7f7b96bf287a1325
ijones16 <jones58ian@gmail.com>
2020-06-23 14:24:19 -0400

    add zen mode and nav-flash

--- .doom.d/+ui.el
+++ .doom.d/+ui.el
@@ -11,21 +11,20 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Open Sans" :size 14))
+(setq doom-font (font-spec :family "Victor Mono" :size 14))
 
-(use-package mixed-pitch
-  :hook
-  ;; If you want it in all text modes:
-  (text-mode . mixed-pitch-mode))
+;; (use-package mixed-pitch
+;;   :hook
+;;   ;; If you want it in all text modes:
+;;   (text-mode . mixed-pitch-mode))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 ;; light theme: white
-(setq doom-theme 'doom-moonlight)
+(setq doom-theme 'doom-palenight)
 
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
-(add-hook 'window-setup-hook #'toggle-frame-fullscreen)
